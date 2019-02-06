ab77dc94fdbfcc5cdf822ab231948d56df726e64
visualstudio <visualstudio@122-PC02>
2019-02-06 10:07:51 -0500

    saa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,31 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static int i=0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MethodA();
+           
+            Console.WriteLine("now I am done!");
+
+        }
+        public static void MethodA()
+        {
+            while (Peanut())
+            {
+                Console.WriteLine("i is {0}", i);
+                if(i>10)
+
+                    {
+                        return;
+
+                    }
+            }
+        }
+        public static bool Peanut()
+        {
+            i++;
+            return true;
         }
     }
 }
